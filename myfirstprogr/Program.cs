c13a32f49c618f4891b2fb45f522bba664772d7c
Carmens <COSMIN@DESKTOP-JAVIBS9>
2023-08-11 11:10:48 +0300

    am rezolvat ex.1, lectia 1

--- myfirstprogr/Program.cs
+++ myfirstprogr/Program.cs
@@ -9,8 +9,34 @@ namespace myfirstprogr
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello you");
+           /*Lab1, ex1: Scrieti un program care citind de la tastatura cele trei dimensiuni ale unui paralelipiped dreptunghic
+            * va afisa volumul lui*/
+
+            
+            
+            
+            Console.WriteLine("Acest program va calcula volumul paralelipipedului dreptunghic");
+
+            Console.WriteLine("Introduceti latimea:");
+           int latime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti lungimea:");
+           int lungime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti inaltimea:");
+            int inaltime = int.Parse(Console.ReadLine());
+
+            int volumulParalelipipeduluiDreptunghic = latime * lungime * inaltime;
+            Console.WriteLine("Volumul paralelipipedului este" + volumulParalelipipeduluiDreptunghic);
+
+
+
+
+
+
+
         }
     }
 }
